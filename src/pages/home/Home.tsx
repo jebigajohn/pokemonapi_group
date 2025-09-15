73c3e006bec1f04d1cea78c02b34c0c138706bf7
Johnstb <jstaub8596@gmail.com>
2025-09-15 11:44:00 +0200

    Home und GenPage dummy

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,16 @@
-import React from "react"
+import React from 'react'
+import { usePokedex } from '../../context/PokemonContext'
+import PokemonCard from '../../component/pokemonCard/PokemonCard'
 
 export default function Home() {
-  return <div>Home</div>
+  const { randomPokemons } = usePokedex()
+
+  if (!randomPokemons) return <div>Loading...</div>
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {randomPokemons.map((p) => (
+        <PokemonCard key={p.id} pokemon={p} onclick={() => {}} />
+      ))}
+    </div>
+  )
 }
