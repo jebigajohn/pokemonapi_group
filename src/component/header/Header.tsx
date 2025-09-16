0d06f520dfb05c54481a47efe5093e5e6b98bb95
nucito101 <andrenucitoschmidt@gmail.com>
2025-09-16 17:09:51 +0200

    add: styling

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,9 +1,23 @@
-import Navbar from '../navbar/Navbar'
+import { useLocation } from "react-router"
+import Navbar from "../navbar/Navbar"
 
 export default function Header() {
+  const location = useLocation()
+
+  const getPageTitle = (pathname: string) => {
+    if (pathname === "/") return "Home"
+    if (pathname.startsWith("/gen/")) return `Generation ${pathname.split("/")[2]}`
+    return "Seite"
+  }
+
+  const subtitle = getPageTitle(location.pathname)
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center mb-10">
       <Navbar />
+      <div className="text-center">
+        <h1 className="mt-7 text-9xl font-solid font-bold text-outline tracking-wider">Pokedex</h1>
+        <p className="text-sm text-gray-500">{subtitle}</p>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       {!randomPokemons ? (
         <div>Loading random Pokémon…</div>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-4">
           {randomPokemons.map((pokemon) => (
             <PokemonCard key={pokemon.id} pokemon={pokemon} />
           ))}
